b6a1dc2766fe46d1b79df7b29fb781a00f2447ad
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-02 21:58:44 +0200

    add exercises class 2

---
+++ alumnos/camr/ejercicios/clase 2/ejercicio 3/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function add() {
+  let result = 6 + 5;
+  return result;
+}
+
+console.log(add());
---
+++ alumnos/camr/ejercicios/clase 2/ejericio 2/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function isPositiveNumber() {
+  const number = 5;
+  const positiveNumber = number >= 0 ? true : false;
+  return positiveNumber;
+}
+
+console.log(isPositiveNumber());
