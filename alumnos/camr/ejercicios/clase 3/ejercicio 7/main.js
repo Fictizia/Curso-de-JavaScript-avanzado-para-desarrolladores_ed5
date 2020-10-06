18468dafaf4428d55245b8e68e0a48d2f4221aa3
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-06 17:08:35 +0200

    add exercise 7

--- alumnos/camr/ejercicios/clase 3/ejercicio 7/main.js
+++ alumnos/camr/ejercicios/clase 3/ejercicio 7/main.js
@@ -2,9 +2,13 @@
 
 function ponCamelCase(str) {
   const separado = str.split(" ");
-  let resultado = " ";
+  let resultado = "";
   for (const palabra of separado) {
-    resultado = `${palabra[0].toUpperCase()}${palabra.slice(1).toLowerCase()}`;
+    resultado += `${palabra[0].toUpperCase()}${palabra.slice(1).toLowerCase()}`;
   }
+
+  resultado = `${resultado[0].toLowerCase()}${resultado.slice(1)}`;
+
   return resultado;
 }
+console.assert(ponCamelCase("Hola a todos que tal") === "holaATodosQueTal");
