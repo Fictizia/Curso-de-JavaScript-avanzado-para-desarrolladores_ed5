55f4616f547c8e0a5e524677b01a01c8df64414f
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-10-07 10:34:47 +0200

    Modify exercise 6 to use Array destructuring

--- alumnos/ssv/Clase 3/ejercicio6.js
+++ alumnos/ssv/Clase 3/ejercicio6.js
@@ -1,19 +1,18 @@
 // 6. Crea una función que reciba un string y lo devuelva con todas las palabras con su primera letra mayúscula.
 
 function ponPrimeraMayuscula(str) {
-  let strArray = str.split(' ');
+  let strArray = str.split(" ");
   let newArray = [];
   let finalStr;
   for (word of strArray) {
-    let capital = word[0].toUpperCase();
-    let others = word.slice(1);
-    newArray.push(capital.concat(others));
-    finalStr = newArray.join(' ');
+    let [capital, ...others] = word;
+    newArray.push(capital.toUpperCase().concat(others.join("")));
+    finalStr = newArray.join(" ");
   }
-
   return finalStr;
 }
-
 console.assert(
-  ponPrimeraMayuscula('En un lugar de la Mancha de cuyo nombre no quiero acordarme') === 'En Un Lugar De La Mancha De Cuyo Nombre No Quiero Acordarme'
+  ponPrimeraMayuscula(
+    "En un lugar de la Mancha de cuyo nombre no quiero acordarme"
+  ) === "En Un Lugar De La Mancha De Cuyo Nombre No Quiero Acordarme"
 );
