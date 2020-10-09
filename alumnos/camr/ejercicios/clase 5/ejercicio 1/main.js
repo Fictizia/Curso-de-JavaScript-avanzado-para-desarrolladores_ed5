99dcb92b173813b629c769b98520b224db9d9cf8
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-09 21:51:34 +0200

    add lesson 5 folder

---
+++ alumnos/camr/ejercicios/clase 5/ejercicio 1/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const estanteria = {
+  libros: [
+    {
+      nombre: "El caballero oscuro",
+      autor: "Frank Miller",
+      leido: false,
+    },
+    {
+      nombre: "El mundo amarillo",
+      autor: "Albert Espinosa",
+      leido: false,
+    },
+    {
+      nombre: "Harry Potter y el caliz de fuego",
+      autor: "J.K. Rowling",
+      leido: true,
+    },
+    {
+      nombre: "El ingenioso hidalgo Don Quijote de la Mancha",
+      autor: "Miguel de Cervantes",
+      leido: false,
+    },
+    {
+      nombre: "Berserk",
+      autor: "Kentaro Miura",
+      leido: true,
+    },
+    {
+      nombre: "Iliada",
+      autor: "Homero",
+      leido: false,
+    },
+  ],
+};
