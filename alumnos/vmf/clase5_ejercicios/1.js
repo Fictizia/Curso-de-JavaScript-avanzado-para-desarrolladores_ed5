920ba6b86a468bb36442cd33abdddab92430e17c
Veronica <vero.fullstack@gmail.com>
2020-10-13 13:41:10 +0200

    ejercicio 1

---
+++ alumnos/vmf/clase5_ejercicios/1.js
@@ -0,0 +1,49 @@
+  /* Como estamos de cuarentena y tenemos que encontrar una forma de pasar el tiempo, vamos a
+probar a ordenar nuestra estantería. Para ello, vamos a crear un método
+interno ordenarPorNombre que ordene los títulos de la librería por su título. */
+
+let estanteria = {
+    libros: [
+      {
+        nombre: 'El caballero oscuro',
+        autor: 'Frank Miller',
+        leido: false
+      },
+      {
+        nombre: 'El mundo amarillo',
+        autor: 'Albert Espinosa',
+        leido: false,
+      },
+      {
+        nombre: 'Harry Potter y el caliz de fuego',
+        autor: 'J.K. Rowling',
+        leido: true,
+      },
+      {
+        nombre: 'El ingenioso hidalgo Don Quijote de la Mancha',
+        autor: 'Miguel de Cervantes',
+        leido: false
+      },
+      {
+        nombre: 'Berserk',
+        autor: 'Kentaro Miura',
+        leido: true
+      },
+      {
+        nombre: 'Iliada',
+        autor: 'Homero',
+        leido: false
+      },
+    ],
+    ordenarPorNombre() {
+      return this.libros.sort((a,b) => {
+        if (a.nombre > b.nombre) {
+          return 1;
+        }
+        if (a.nombre < b.nombre) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+  }
