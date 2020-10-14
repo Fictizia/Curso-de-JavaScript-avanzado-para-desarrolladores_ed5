2495bf8bf0bbe8e789463412297f6b202d7e08b0
Concha Asensio <conchaasensiomr@gmail.com>
2020-10-14 14:13:25 +0200

    update exercises

--- alumnos/camr/ejercicios/clase 2/ejericio 2/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function isPositiveNumber() {
-  const number = 5;
-  const positiveNumber = number >= 0 ? true : false;
-  return positiveNumber;
-}
-
-console.log(isPositiveNumber());
--- alumnos/camr/ejercicios/clase 4/ejercicio 11/main.js
+++ alumnos/camr/ejercicios/clase 4/ejercicio 11/main.js
@@ -1,5 +1,1 @@
 "use strict";
-
-function getCircleArea(radio) {
-  return Math.PI * Math.pow(radio, 2); // Math.PI * radio ** 2;
-}
