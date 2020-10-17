a75e79da836dfecef9c967435098cdd241f0c469
Carlos Ruiz <carl.45.cr@gmail.com>
2020-10-17 23:14:31 +0200

    add: exercises class 7

--- alumnos/crc/ejercicios-clase-7/teachers.js
+++ alumnos/crc/ejercicios-clase-7/teachers.js
@@ -7,12 +7,12 @@ const profesores = await getProfesores();
 
 
 const articleRegex = /(<\s*article[^>]*>(.|\n)*?<\s*\/\s*article>)/gm
-const imageRegex = /(<img.*?src="(.*?)".*?alt="(.*?)")/
+const imageRegex = /<img.*?src="(.*?)".*?alt="(.*?)">/
 
 const articlesElements = profesores.match(articleRegex)
 
 const teachers = articlesElements.map(articleElement => {
-  const [, , image, name] = articleElement.match(imageRegex)
+  const [, image, name] = articleElement.match(imageRegex)
   return {
     name,
     image
