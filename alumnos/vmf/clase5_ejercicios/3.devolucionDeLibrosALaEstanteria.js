9bd30dccdb67cbf9b45ddb97bee3532c50d48a95
Veronica <vero.fullstack@gmail.com>
2020-10-22 08:42:12 +0200

    correcion de ejercicios clase 5

--- alumnos/vmf/clase5_ejercicios/3.devolucionDeLibrosALaEstanteria.js
+++ alumnos/vmf/clase5_ejercicios/3.devolucionDeLibrosALaEstanteria.js
@@ -3,56 +3,55 @@ devolvérnoslos antes de tiempo. Modificar esta variable interna para que cuando
 pase a valer false los libros leidos vuelvan a la estanteria. No hace falta mantener el orden. */
 
 let estanteria = {
-    libros: [
-      {
-        nombre: 'El caballero oscuro',
-        autor: 'Frank Miller',
-        leido: false
-      },
-      {
-        nombre: 'El mundo amarillo',
-        autor: 'Albert Espinosa',
-        leido: false,
-      },
-      {
-        nombre: 'Harry Potter y el caliz de fuego',
-        autor: 'J.K. Rowling',
-        leido: true,
-      },
-      {
-        nombre: 'El ingenioso hidalgo Don Quijote de la Mancha',
-        autor: 'Miguel de Cervantes',
-        leido: false
-      },
-      {
-        nombre: 'Berserk',
-        autor: 'Kentaro Miura',
-        leido: true
-      },
-      {
-        nombre: 'Iliada',
-        autor: 'Homero',
-        leido: false
-      },
-    ],
-    prestamo: false,
-    librosPrestados: [],
-    prestamo(flag) {
-      this.prestamo = flag;
-      if (this.prestamo) {
-        this.libros.forEach((libro, i) => {
-          if(libro.leido) {
-            let box = this.libros.splice(i, 1);
-            this.librosPrestados = this.librosPrestados.concat(box);
-          }
-        })
-        console.log(this.librosPrestados);
-      } else if (this.librosPrestados.length !== 0) {
-        this.libros.concat(this.librosPrestados);
-        this.librosPrestados = [];
-        console.log(this.libros);
-      }
+  libros: [
+    {
+      nombre: 'El caballero oscuro',
+      autor: 'Frank Miller',
+      leido: false
+    },
+    {
+      nombre: 'El mundo amarillo',
+      autor: 'Albert Espinosa',
+      leido: false,
+    },
+    {
+      nombre: 'Harry Potter y el caliz de fuego',
+      autor: 'J.K. Rowling',
+      leido: true,
+    },
+    {
+      nombre: 'El ingenioso hidalgo Don Quijote de la Mancha',
+      autor: 'Miguel de Cervantes',
+      leido: false
+    },
+    {
+      nombre: 'Berserk',
+      autor: 'Kentaro Miura',
+      leido: true
+    },
+    {
+      nombre: 'Iliada',
+      autor: 'Homero',
+      leido: false
+    },
+  ],
+  librosPrestados: [],
+  set prestamo(flag) {
+    if (flag) {
+      this.libros.forEach((libro, i) => {
+        if(libro.leido) {
+          let box = this.libros.splice(i, 1);
+          this.librosPrestados = this.librosPrestados.concat(box);
+        }
+      })
+      console.log(this.librosPrestados);
+    } else if (this.librosPrestados.length !== 0) {
+      this.libros = this.libros.concat(this.librosPrestados);
+      this.librosPrestados = [];
+      console.log("de vuelta!: ", this.libros);
     }
   }
+}
 
-estanteria.prestamo(false);
+estanteria.prestamo = true;
+estanteria.prestamo = false;
